100ea7b353875f1ccee9d287d8efeee2f5c773b3
Dom <dom@magfest.org>
2014-05-13 15:13:15 -0400

    initial config for fabric/puppet uber deploy

---
+++ fabfile.py
@@ -0,0 +1,12 @@
+from fabric.api import *
+from fabric.contrib.project import rsync_project
+
+def apply():
+    rsync_project(remote_dir='/usr/local/puppet', local_dir='.', extra_opts='--delete')
+    sudo('puppet apply --modulepath /usr/local/puppet/modules /usr/local/puppet/manifests/site.pp')
+
+def setup_client():
+    sudo('apt-get update')
+    sudo('apt-get install puppet')
+    sudo('mkdir -p /usr/local/puppet')
+    sudo('chown -R %s /usr/local/puppet' % env.user)
