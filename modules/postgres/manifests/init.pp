13bae5eee8ba5f1ee44ecefedb48f2ceae8cc9da
Dom <dom@magfest.org>
2014-05-14 00:43:13 -0400

    first pass on postgresql server config

--- modules/postgres/manifests/init.pp
+++ modules/postgres/manifests/init.pp
@@ -1,5 +1,14 @@
-class postgres{
-  include postgres::install, postgres::config, postgres::service
+class { 'postgresql::server':
+  ip_mask_deny_postgres_user  => '0.0.0.0/32',
+  ip_mask_allow_all_users   => '0.0.0.0/0',
+  listen_addresses        => '127.0.0.1',
+  #ipv4acls              => ['hostssl all johndoe1 192.168.0.0/24 cert'],
+  manage_firewall     => true,
+  #postgres_password => 'TPSrep0r234t!',
+}
+
+class postgres {
+  include postgresql::server, postgres::install, postgres::config
 }
 
 class postgres::install{
@@ -9,14 +18,10 @@ class postgres::install{
 }
 
 class postgres::config{
-
-}
-
-class postgres::service{
-  service {'postgresql':
-    ensure         => running,
-    enable => true,
+  postgresql::server::db { 'm13':
+    user         => 'm13',
+    password => postgresql_password('m13', 'm13'),
   }
 }
 
-Class["postgres::install"] -> Class["postgres::config"] -> Class["postgres::service"]
+Class["postgres::install"] -> Class["postgres::config"] -> Class["postgressql::server"]
