6fb5975ca94d9bb1101492c63b14e76fa9485df5
Jonathan Harker <code@gentlydownthe.net>
2014-05-15 00:41:07 -0400

    Make the uber-db class a bit more sane
    Let's put all of the uber-db stuff in a single parameterized class and
    create a couple different nodes that pass in different paramters.

--- puppet/modules/uber-db/manifests/init.pp
+++ puppet/modules/uber-db/manifests/init.pp
@@ -1,27 +1,40 @@
-class { 'postgresql::server':
-  ip_mask_deny_postgres_user  => '0.0.0.0/32',
-  ip_mask_allow_all_users   => '0.0.0.0/0',
-  listen_addresses        => '127.0.0.1',
-  #ipv4acls              => ['hostssl all johndoe1 192.168.0.0/24 cert'],
-  manage_firewall     => true,
-  #postgres_password => 'TPSrep0r234t!',
-}
+# Class uber-db
+#
+# Document the class here
+#
+# No newline after comment
+class uber-db (
+  $user = 'default_user',
+  $pass = 'default_pass',
+) {
 
-class uber-db {
-  include postgresql::server, postgres::install, postgres::config
-}
+  package { 'postgresql':
+    ensure => present,
+  }
+  package { 'postgresql-contrib':
+    ensure => present,
+  }
+  package { 'libpq-dev':
+    ensure => present,
+  }
 
-class postgres::install{
-  package { 'postgresql':  ensure        => present }
-  package { 'postgresql-contrib': ensure => present }
-  package { 'libpq-dev': ensure          => present }
-}
-
-class postgres::config{
   postgresql::server::db { 'm13':
-    user         => 'm13',
-    password => postgresql_password('m13', 'm13'),
+    user     => $user,
+    password => postgresql_password($user, $pass),
+    require  => [
+      Package['postgresql'],
+      Package['postgresql-contrib'],
+      Package['libpq-dev'],
+    ],
+  }
+
+  class { 'postgresql::server':
+    ip_mask_deny_postgres_user => '0.0.0.0/32',
+    ip_mask_allow_all_users    => '0.0.0.0/0',
+    listen_addresses           => '127.0.0.1',
+    #ipv4acls                  => ['hostssl all johndoe1 192.168.0.0/24 cert'],
+    manage_firewall            => true,
+    #postgres_password         => 'TPSrep0r234t!',
+    require                    => Postgresql::Server::Db['m13'],
   }
 }
-
-Class["postgres::install"] -> Class["postgres::config"] -> Class["postgressql::server"]
