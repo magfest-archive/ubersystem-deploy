aa1e010173eefc082b884c11850f14022b5aac32
Dom <dom@magfest.org>
2014-05-15 18:52:18 -0400

    finished uber config section, cleaned up some more. still WIP, doesn't apply right

--- puppet/modules/uber/manifests/python.pp
+++ puppet/modules/uber/manifests/python.pp
@@ -1,10 +1,5 @@
-# TODO: add in database user/password/dbname and write the 
-# production.conf file for ubersystem
-
 # TODO: probably rename this file from python to ubersystem
 # or, move the ubersystem-specific stuff out of here.
-
-# TODO: dont hardcode venv path, can we assign it inside the class itself
 
 # TODO: hostname as a paramater
 
@@ -22,13 +17,13 @@ class uber::python (
   $db_name = 'm13',
 
   $socket_port = '4321',
-  $socket_hostname = '0.0.0.0',
+  $socket_host = '0.0.0.0',
+  $hostname = '', # defaults to hostname of the box
   $ubersystem_url_prefix = '/magfest',
+
 ) {
 
   $python_ver = '3'
-  $venv_path = "${uber_path}/env"
-  $venv_bin = "${uber_path}/bin"
 
   class { '::python':
     # ensure     => present,
@@ -53,11 +48,22 @@ class uber::python (
     notify   => Exec['uber_virtualenv'],
   }
 
+  $hostname_to_use = $hostname ? {
+    ''      => $fqdn,
+    default => $hostname,
+  }
+
+  $url_root = "http://${hostname_to_use}:${socket_port}/${ubersystem_url_prefix}"
+
   $python_cmd = $python_ver ? {
     '2'     => 'python2',
     '3'     => 'python3',
     default => fail("Bad python version: ${python_ver}"),
   }
+
+  $venv_path = "${uber_path}/env"
+  $venv_bin = "${venv_path}/bin"
+  $venv_python = "${venv_path}/python"
 
   # seems puppet's virtualenv support is broken for python3, so roll our own
   exec { 'uber_virtualenv':
@@ -65,11 +71,12 @@ class uber::python (
     cwd         => $uber_path,
     path        => '/usr/bin',
     refreshonly => true,
+    creates     => $venv_python,
     notify      => Exec['uber_distribute_setup'],
   }
 
   exec { 'uber_distribute_setup' :
-    command     => "${venv_bin}/python distribute_setup.py",
+    command     => "${venv_python} distribute_setup.py",
     cwd         => "${uber_path}",
     refreshonly => true,
     notify      => File["${uber_path}/production.conf"],
@@ -84,15 +91,16 @@ class uber::python (
   }
 
   exec { 'uber_setup' :
-    command     => "${venv_bin}/python setup.py develop",
+    command     => "${venv_python} setup.py develop",
     cwd         => "${uber_path}",
     refreshonly => true,
+    creates     => "${venv_path}/lib/python3.4/site-packages/CherryPy-3.2.4-py3.4.egg/",
     notify      => Exec['uber_init_db'],
   }
 
   # TODO: dont always do this
   exec { 'uber_init_db' :
-    command     => "${venv_bin}/python uber/init_db.py",
+    command     => "${venv_python} uber/init_db.py",
     cwd         => "${uber_path}",
     refreshonly => true,
   }
