661297ccac2a6782f8c905f575de685c775de0f4
Dom <dom@magfest.org>
2014-05-17 23:57:10 -0400

    make apt-get update on initial install press Y if asked

--- puppet/fabfile.py
+++ puppet/fabfile.py
@@ -8,6 +8,6 @@ def apply():
 def setup_client():
     sudo('hostname staging.magfest.net') # TODO dont hardcode
     sudo('apt-get update')
-    sudo('apt-get install puppet')
+    sudo('apt-get -y install puppet')
     sudo('mkdir -p /usr/local/puppet')
     sudo('chown -R %s /usr/local/puppet' % env.user)
