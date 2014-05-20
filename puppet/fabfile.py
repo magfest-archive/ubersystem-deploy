d65ca5de58a1d1953f43ee2ef3dabc8775ad74ea
Dom <dom@magfest.org>
2014-05-19 20:55:29 -0400

    WIP hiera support. YAML files are busted, but everything else is working OK to support it. Note: in the future, we won't be checking the node-specific .yaml files in here, instead they'll be in a local private git repo of their own since they will contain secrets/etc

--- puppet/fabfile.py
+++ puppet/fabfile.py
@@ -1,17 +1,33 @@
 from fabric.api import *
 from fabric.contrib.project import rsync_project
 
-
-def sync():
-    rsync_project(remote_dir='/usr/local/puppet', local_dir='.', extra_opts='--delete')
+puppet_dir = '/usr/local/puppet'
+puppet_conf = puppet_dir+'/puppet.conf'
+hiera_conf = puppet_dir+'/hiera/hiera.yaml'
+manifest_to_run = puppet_dir+'/manifests/site.pp'
+modules_path = puppet_dir+'/modules'
 
 def apply():
     execute(sync)
-    sudo('puppet apply --modulepath /usr/local/puppet/modules /usr/local/puppet/manifests/site.pp')
 
-def setup_client():
-    sudo('hostname staging.magfest.net') # TODO dont hardcode
+    # copy a template of the puppet.conf file, add the line to it
+    # that we need to run hiera correctly
+    sudo('cp -f '+puppet_dir+'/puppet.conf.template '+puppet_conf)
+    sudo('echo -en "[main]\nhiera_config='+hiera_conf+'" >> '+puppet_conf)
+
+    sudo(   "puppet apply "
+            "--config "+puppet_conf+" "
+            "--modulepath "+modules_path+" "
+            " "+manifest_to_run+" "
+            )
+
+def sync():
+    rsync_project(remote_dir=puppet_dir, local_dir='.', extra_opts='--delete')
+
+
+def bootstrap_new_server():
+    sudo('hostname ' + env.host_string)
     sudo('apt-get update')
     sudo('apt-get -y install puppet')
-    sudo('mkdir -p /usr/local/puppet')
-    sudo('chown -R %s /usr/local/puppet' % env.user)
+    sudo('mkdir -p ' + puppet_dir)
+    sudo('chown -R %s ' + puppet_dir + '' % env.user)
