4fb8ea780839fcd13bf0e6fdcc689721619f8a03
Dom <dom@magfest.org>
2014-05-20 19:51:20 -0400

    make fabric only deploy the hiera file we care about

--- puppet/fabfile.py
+++ puppet/fabfile.py
@@ -4,8 +4,29 @@ from fabric.contrib.project import rsync_project
 puppet_dir = '/usr/local/puppet'
 puppet_conf = puppet_dir+'/puppet.conf'
 hiera_conf = puppet_dir+'/hiera/hiera.yaml'
+node_dir = puppet_dir+'/hiera/nodes'
 manifest_to_run = puppet_dir+'/manifests/site.pp'
 modules_path = puppet_dir+'/modules'
+
+rsync_opts = '--delete -L --exclude=.git'
+
+def sync():
+    # 1st sync everything but the nodes dir
+    rsync_project(
+            remote_dir=puppet_dir, 
+            local_dir='.', 
+            extra_opts=rsync_opts + ' --exclude=hiera/nodes'
+    )
+    
+    sudo('mkdir ' +node_dir)
+
+    # now sync just the hiera node we're looking at
+    # (we don't want to sync them all because these node files contain secrets)
+    rsync_project(
+            remote_dir=node_dir,
+            local_dir='./hiera/nodes/' + env.host_string + '.yaml',
+            extra_opts=rsync_opts
+    )
 
 def apply():
     execute(sync)
@@ -21,9 +42,7 @@ def apply():
             " "+manifest_to_run+" "
             )
 
-def sync():
-    rsync_project(remote_dir=puppet_dir, local_dir='.', extra_opts='--delete')
-
+    # TODO: delete the node config since it contains secret info
 
 def bootstrap_new_server():
     sudo('hostname ' + env.host_string)
