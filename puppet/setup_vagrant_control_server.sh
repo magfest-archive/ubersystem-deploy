88cdcb50c4fcc568003a34a8d532af0da33a58ae
Dom <dom@magfest.org>
2014-10-09 16:18:44 -0400

    make apt-get run in an automated fashion

--- puppet/setup_vagrant_control_server.sh
+++ puppet/setup_vagrant_control_server.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 sudo apt-get update -y
-sudo apt-get install fabric vim lynx git tofrodos
+sudo apt-get install -y fabric vim lynx git tofrodos
 
 fab -u root -H `hostname` bootstrap_vagrant_control_server
