3b0e44272332b450dc797e1163a6c75fd172dc60
Dom <dom@magfest.org>
2014-10-09 11:43:21 -0400

    working pass on vagrant support for uber deploys - TODO: index.html files written out in the wrong order, must fix. minor thing. - generated index.html lists any deployed ubersystem instances on that box  - symlink fix ported over to workaround Vagrant+python virtualenv bug  - move fake SSL certs into uber module

--- vagrant/vagrant.sh
+++ vagrant/vagrant.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
+# there is a bug in python -m venv which doesnt obey --copies (i.e. don't do any symlinks)
+# I have submitted this to CPython for inclusion but for now, patch the installed python file.
+# (we can't use symlinks with SMB shares)
+sudo patch /usr/lib/python3.4/venv/__init__.py < /home/vagrant/uber/vagrant/venv-symlink-fix.patch
+
 # . puppet/setup_control_server.sh
