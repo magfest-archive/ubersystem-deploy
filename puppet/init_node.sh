f8118e5b51439da010cbdafada6be044268d189b
Dom <dom@magfest.org>
2014-10-11 00:48:48 -0400

    fix init_node

--- puppet/init_node.sh
+++ puppet/init_node.sh
@@ -12,4 +12,4 @@ hostn=$1
 # which can then be used to do stuff. only run this script once, and once
 # it's successful, use apply instead
 
-fab -u root -H $hostn apply_new_node
+fab -u root -H $hostn puppet_apply_new_node
