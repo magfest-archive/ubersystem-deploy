41081a3987f1a1a9ff6afcb13aa811440ba458bb
Dominic Cerquetti <binary1230@gmail.com>
2015-06-26 17:10:07 -0500

    don't do the symlink stuff anymore

--- vagrant/vagrant.sh
+++ vagrant/vagrant.sh
@@ -6,10 +6,5 @@ sudo apt-get install -y wamerican language-pack-id
 sudo locale-gen en_US en_US.UTF-8 hu_HU hu_HU.UTF-8
 sudo dpkg-reconfigure locales
 
-# there is a bug in python -m venv which doesnt obey --copies (i.e. don't do any symlinks)
-# I have submitted this to CPython for inclusion but for now, patch the installed python file.
-# (we can't use symlinks with SMB shares)
-sudo patch /usr/lib/python3.4/venv/__init__.py < /home/vagrant/uber/vagrant/venv-symlink-fix.patch
-
 # setup our custom bash aliases to make development easy
 cp /home/vagrant/uber/vagrant/bash_aliases /home/vagrant/.bash_aliases
