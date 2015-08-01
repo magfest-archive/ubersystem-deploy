cdb946cfded89c9e34ddf82d16c4f1dd9534c8c0
Dominic Cerquetti <dom@magfest.org>
2015-08-01 10:25:34 -0400

    psql support

--- puppet/manifests/roles.pp
+++ puppet/manifests/roles.pp
@@ -3,6 +3,13 @@ class roles {
 }
 
 class roles::uber_server () inherits roles {
+  class { 'postgresql::server':
+    ip_mask_deny_postgres_user => '0.0.0.0/32',
+    ip_mask_allow_all_users    => '0.0.0.0/0',
+    listen_addresses           => 'localhost',
+    manage_firewall            => true,
+  }
+
   include uber
   include uber::rams_app_stack
 }
