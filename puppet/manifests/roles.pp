55f3878a9a3e8e4cf474b65be1190a3bbd335e62
Dominic Cerquetti <dom@magfest.org>
2015-08-02 11:39:07 -0400

    add in support for hiera() looks up in hiera files themselves updated shortcuts add in uber::nginx into rams_app_stack

--- puppet/manifests/roles.pp
+++ puppet/manifests/roles.pp
@@ -1,8 +1,23 @@
-class roles {
+import 'ssh.pp'
 
+class role_common (
+  $timezone,
+) {
+  include ssh
+
+  include '::ntp'
+
+  class { 'timezone':
+    timezone => $timezone,
+  }
+
+  # users in this group can sudo
+  group { 'admin':
+    ensure => present
+  }
 }
 
-class roles::uber_server () inherits roles {
+class roles::uber_server () inherits role_common {
   class { 'postgresql::server':
     ip_mask_deny_postgres_user => '0.0.0.0/32',
     ip_mask_allow_all_users    => '0.0.0.0/0',
@@ -10,6 +25,8 @@ class roles::uber_server () inherits roles {
     manage_firewall            => true,
   }
 
+  include nginx
   include uber
+
   include uber::rams_app_stack
 }
