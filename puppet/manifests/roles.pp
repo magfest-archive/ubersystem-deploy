cb687a96daa6dc843e2edbdcb7b651544865b486
Dominic Cerquetti <dom@magfest.org>
2015-08-04 11:08:01 -0400

    set this back to normal

--- puppet/manifests/roles.pp
+++ puppet/manifests/roles.pp
@@ -18,7 +18,6 @@ class role_common (
 }
 
 class roles::uber_server () inherits role_common {
-  /* REAL, USE THIS
   class { 'postgresql::server':
     ip_mask_deny_postgres_user => '0.0.0.0/32',
     ip_mask_allow_all_users    => '0.0.0.0/0',
@@ -29,8 +28,5 @@ class roles::uber_server () inherits role_common {
   include nginx
   include uber
 
-  include uber::profile_rams_full_stack*/
-
-  # TEST ONLY, DONT CHECK IN
-  include uber::app
+  include uber::profile_rams_full_stack
 }
